6c0fbb7852632f21e032a03defde9409a34262b3
Alexander Espinosa <alexander.edward.espinosa@gmail.com>
2021-08-26 13:48:31 -0500

    ERD

--- table_creation.sql
+++ table_creation.sql
@@ -1,8 +1,41 @@
 -- Create a departments table 
 
-Create TABLE (
+Drop TABLE departments;
+
+Create TABLE departments(
 	dept_no VARCHAR (20),
-	dept_name VARCHAR (45) NOT NULL,
-	
-	
-)
+	dept_name VARCHAR (30) NOT NULL,
+	Primary Key (dept_no)
+);
+
+select * from departments;
+
+-- Create a employees table 
+
+Drop TABLE employees;
+
+Create TABLE employees(
+	emp_no INT NOT NULL,
+	emp_title VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	Sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	Primary Key (emp_no)
+);
+
+select * from employees;
+
+-- Create a dept_emp table 
+
+Drop TABLE dept_emp;
+
+Create TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+select * from dept_emp;
